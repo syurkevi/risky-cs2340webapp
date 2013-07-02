e670ffd6bf7f1607132c0c16c7c1e3b9ebcac8f3
Jonathan H <jonathantrevorh@gmail.com>
2013-07-02 19:48:16 -0400

    R4.2

---
+++ src/main/java/edu/gatech/cs2340/risky/models/TerritoryDeed.java
@@ -0,0 +1,11 @@
+package edu.gatech.cs2340.risky.models;
+
+import edu.gatech.cs2340.risky.Model;
+
+public class TerritoryDeed extends Model {
+    public int armies;
+    
+    public TerritoryDeed() {
+        
+    }
+}
